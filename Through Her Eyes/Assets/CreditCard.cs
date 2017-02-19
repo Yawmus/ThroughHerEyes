2205964eee6b16fca92f8d7fe955b19db778a87d
Yawmus <peter.jacobsen55@gmail.com>
2017-02-18 21:05:46 -0700

    Putting project on github

---
+++ Through Her Eyes/Assets/CreditCard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditCard : MonoBehaviour {
+
+	float timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.fixedDeltaTime;
+		if (timer > 25f)
+			Application.LoadLevel ("MainMenu");
+	}
+}
---
+++ Through Her Eyes/Assets/Dialogue.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class Dialogue : MonoBehaviour {
+
+	Queue<string> buffer = new Queue<string>();
+	Text t, p, x;
+	GameObject bg1, bg2;
+	float timer = 0;
+
+	// Use this for initialization
+	void Awake () {
+		bg1 = transform.FindChild ("Container1").gameObject;
+		bg2 = transform.FindChild ("Container2").gameObject;
+		p = bg1.transform.FindChild("BG1").FindChild ("Person").GetComponent<Text>();
+		t = bg1.transform.FindChild("BG2").FindChild ("Text").GetComponent<Text>();
+		x = bg2.transform.FindChild("BG3").FindChild ("Text").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer >= 2.7f)
+		{
+			if(buffer.Count != 0)
+			{
+				string str = buffer.Dequeue();
+				string[] temp = Regex.Split(str, ": ");
+				if(temp.Length == 1)
+				{
+					bg2.SetActive(true);
+					bg1.SetActive(false);
+					x.text = str;
+				}
+				else
+				{
+					bg1.SetActive(true);
+					bg2.SetActive(false);
+					p.text = temp[0];
+					t.text = temp[1];
+				}
+				timer = 0;
+			}
+			else
+			{
+				bg1.SetActive(false);
+				bg2.SetActive(false);
+			}
+		}
+	}
+
+	public void Say(string message)
+	{
+		buffer.Enqueue (message);
+	}
+}
